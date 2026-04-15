9d2fbe50ff651b518db0f4b114cb03b8b594e6fa
Quoc <quocxibo123@gmail.com>
2026-04-15 08:27:59 +0700

    Thêm chức năng tải lên avatar người dùng

--- frontend/src/api/user.api.js
+++ frontend/src/api/user.api.js
@@ -6,6 +6,16 @@ const userApi = {
   getProfile: () => axiosClient.get('/user/profile.php'),
 
   updateProfile: (data) => axiosClient.post('/user/update_profile.php', data),
+
+  uploadAvatar: (file) => {
+    const formData = new FormData();
+    formData.append('avatar', file);
+    return axiosClient.post('/user/upload_avatar.php', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
 };
 
 export default userApi;
